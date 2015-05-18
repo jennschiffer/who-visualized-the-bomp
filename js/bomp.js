8f4e42dc7150c90604ab61705a247c78a30e6530
Jenn Schiffer <jenn@dotbiz.info>
2015-05-18 13:29:22 -0400

    pt 5 adds web audio api and midi, start pause music and moves

--- js/bomp.js
+++ js/bomp.js
@@ -14,6 +14,14 @@ $(function(){
     bopHead: 'bop-head'
   }
   
+  var playSound, dataArray, bufferLength, count;
+  var midiBuffer = null;
+  var audioURL = 'audio/aha.mp3';
+  var audioTime = 0;
+  var audioOffset = 0;
+  var audioPlaying = false;
+  var currentMoves = '';
+  
   /* hot dance moves */
   
   var marquee = function(){
@@ -40,12 +48,58 @@ $(function(){
   
   
   
+  /* web audio fun */
+  
+  var loadAudio = function(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', audioURL, true);
+    request.responseType = 'arraybuffer';
+  
+    request.onload = function() {
+      audioCtx.decodeAudioData(request.response, function(buffer) {
+        midiBuffer = buffer;
+      });
+      
+      // debug - load audio
+      console.log('audio loaded');
+    }
+    request.send();
+  };
+
+  
+  var playSong = function(){
+    audioTime = audioCtx.currentTime;
+    playSound = audioCtx.createBufferSource();
+    playSound.buffer = midiBuffer;
+    playSound.connect(audioCtx.destination);
+    playSound.start(0, audioOffset);
+    
+    playSound.connect(analyser);
+    
+    DOM.dancers.attr('class', currentMoves);
+  };
+  
+  var pauseSong = function(){
+    playSound.stop();
+    audioOffset += audioCtx.currentTime - audioTime;
+    currentMoves = DOM.dancers.attr('class');
+  };
+  
+  // web audio api turn upppp
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  var audioCtx = new AudioContext();
+  var analyser = audioCtx.createAnalyser();
+  
+  loadAudio(audioURL);
+  
+  
+  
   /* key events */
   
   $('body').keypress(function(e){
     
     // debug - print key code
-    console.log(e.which);
+    // console.log(e.which);
     
     // choose action depending on key code
     switch ( e.which ) {
@@ -74,8 +128,25 @@ $(function(){
         // space 
         stopDancing();
         break;
+        
+      case 102:
+        // f
+        if (!audioPlaying) { 
+          audioPlaying = true;
+          playSong();
+        }
+        break;
+        
+      case 114:
+        // r
+        if (audioPlaying) {
+          audioPlaying = false;
+          pauseSong();
+          stopDancing();
+        }
+        break;
     }
     
   });
-  
+
 });
