31970b75e19fd9a699f977cf98a16b692ad3f8ea
Jenn Schiffer <jenn@dotbiz.info>
2015-05-17 23:51:12 -0400

    pt 3, add marquee and also init web rebels slides

--- js/bomp.js
+++ js/bomp.js
@@ -1,3 +1,48 @@
 $(function(){
-  console.log('lol');
+  
+  var DOM = {
+    dancers: $('#dancers'),
+    dancerLeft: $('.dancer.left'),
+    dancerRight: $('.dancer.right'),
+  };
+  
+  var classes = {
+    marquee: 'marquee'
+  }
+  
+  /* hot dance moves */
+  
+  var marquee = function(){
+    DOM.dancers.addClass(classes.marquee);
+  };
+  
+  var stopDancing = function(){
+    DOM.dancers.removeClass(classes.marquee);
+  };
+  
+  
+  
+  /* key events */
+  
+  $('body').keypress(function(e){
+    
+    // debug - print key code
+    console.log(e.which);
+    
+    // choose action depending on key code
+    switch ( e.which ) {
+      
+      case 0:
+        // arrow 
+        marquee();
+        break;
+      
+      case 32:
+        // space 
+        stopDancing();
+        break;
+    }
+    
+  });
+  
 });
