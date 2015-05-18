9c92e8bb11ba18cc011675f7b48e9fbc56471840
Jenn Schiffer <jenn@dotbiz.info>
2015-05-18 12:10:09 -0400

    pt 4 adds more dance moves

--- js/bomp.js
+++ js/bomp.js
@@ -7,17 +7,35 @@ $(function(){
   };
   
   var classes = {
-    marquee: 'marquee'
+    marquee: 'marquee',
+    bendArms: 'bend-arms',
+    bendLegs: 'bend-legs',
+    sideKick: 'side-kick',
+    bopHead: 'bop-head'
   }
   
   /* hot dance moves */
   
   var marquee = function(){
-    DOM.dancers.addClass(classes.marquee);
+    DOM.dancers.toggleClass(classes.marquee);
+  };
+  
+  var bopHead = function(){
+    DOM.dancers.toggleClass(classes.bopHead);
+  }
+    
+  var bendArms = function(){
+    DOM.dancers.toggleClass(classes.bendArms);
+  };
+  
+  var bendLegs = function(){
+    // make sure legs are stationary first
+    DOM.dancers.removeClass(classes.sideKick).toggleClass(classes.bendLegs);
   };
   
   var stopDancing = function(){
-    DOM.dancers.removeClass(classes.marquee);
+    // remove all classes
+    DOM.dancers.attr('class', null);
   };
   
   
@@ -35,6 +53,21 @@ $(function(){
       case 0:
         // arrow 
         marquee();
+        break;
+        
+      case 106:
+        // j
+        bopHead();
+        break;
+        
+      case 101:
+        // e
+        bendArms();
+        break;
+        
+      case 110:
+        // n
+        bendLegs();
         break;
       
       case 32:
